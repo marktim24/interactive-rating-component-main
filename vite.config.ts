6af5649ccf9d57e1497293b46df4403d9d4a1084
Mark <timoshevich199524@mail.ru>
2023-03-04 16:03:31 +0200

    Dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/interactive-rating-component',
+	base: '/interactive-rating-component/',
 	plugins: [react()],
 })
